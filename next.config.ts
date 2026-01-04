07da6cbcb8f0748bdde23416678921c4a25a45a6
UN1TOne <devunitone@gmail.com>
2026-01-04 20:43:40 +0900

    fix: change the next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = "Robot-Arm-Manipulator";
+
 const nextConfig: NextConfig = {
   reactStrictMode: false,
+
+  output: 'export',
+
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
 };
 
 export default nextConfig;
